1904640bae9b630df185e70006d6cc8178d98815
iyed8 <164577765+iyedGrs@users.noreply.github.com>
2024-09-04 19:39:18 +0100

    front NavBar

--- Front/src/components/NavBarPatientDoc.jsx
+++ Front/src/components/NavBarPatientDoc.jsx
@@ -10,45 +10,46 @@ const NavBarPatientDoc = ({ setSideBarOpen, sideBarOpen }) => {
       <div>
         <p className="text-md md:te font-semibold ">Pages/Dashboard</p>
       </div>
-      <div className="flex items-center gap-4">
-        <div className="block md:hidden">
-          <button
-            onClick={() => setSideBarOpen(!sideBarOpen)}
-            className="p-2 text-white"
-          >
-            {/* Icon for Hamburger Menu */}
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
-        </div>
-
-        <div className="bg-white p-2 rounded-full flex items-center shadow-md">
+      <div className=" flex items-center justify-between    gap-4">
+        <div className="left bg-white p-2  rounded-full flex items-center shadow-md">
           <SearchIcon className="text-black" />
           <input
-            className="border-none text-black outline-none rounded ml-2 w-30 sm:w-64"
+            className="border-none text-black outline-none rounded ml-2 w-30 sm:w-64 md:w-44 lg:w-96"
             type="text"
             placeholder="Search"
           />
         </div>
-        <div className=" p-2 rounded-full flex items-center ">
-          <Person2Icon className="text-white" />
-          <span className="ml-2 hidden sm:block">Sign in</span>
-        </div>
-        <div className="flex gap-2">
-          <SettingsIcon className="text-white hover:text-gray-400 transition-colors duration-300" />
-          <NotificationsIcon className="text-white hover:text-gray-400 transition-colors duration-300" />
+        <div className="right flex items-center   w-full justify-end">
+          <div className=" p-2 rounded-full flex items-center ">
+            <Person2Icon className="text-white" />
+            <span className="ml-2 hidden md:hidden lg:block">Sign in</span>
+          </div>
+          <div className="flex gap-2">
+            <SettingsIcon className="text-white hover:text-gray-400 transition-colors duration-300" />
+            <NotificationsIcon className="text-white hover:text-gray-400 transition-colors duration-300" />
+          </div>
+          <div className="block ">
+            <button
+              onClick={() => setSideBarOpen(!sideBarOpen)}
+              className="p-2 text-white block lg:hidden md:hidden"
+            >
+              {/* Icon for Hamburger Menu */}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
