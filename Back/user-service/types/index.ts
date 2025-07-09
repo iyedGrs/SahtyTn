52c8aeb554d3b9ae451de726e5f1a6fef5e1248e
iyed8 <164577765+iyedGrs@users.noreply.github.com>
2025-07-09 23:52:08 +0100

    feat: create user-service dedicated types
    - Extract user-related types to user-service/types/
    - Add IUser, IUserResponse, IUserRegistration, IUserLogin interfaces
    - Add IAuthResponse and IJwtPayload types
    - Extend types from common IBaseEntity interface

---
+++ Back/user-service/types/index.ts
@@ -0,0 +1,43 @@
+import { IBaseEntity } from "../../common-libs/types";
+
+export interface IUser extends IBaseEntity {
+  username?: string;
+  email: string;
+  password: string;
+  date: string;
+  role: string;
+  id_doctor?: string | null;
+}
+
+export interface IUserResponse extends IBaseEntity {
+  username?: string;
+  email: string;
+  date: string;
+  role: string;
+  id_doctor?: string | null;
+}
+
+export interface IUserRegistration {
+  username?: string;
+  email: string;
+  password: string;
+  date: string;
+  role: string;
+  id_doctor?: string;
+}
+
+export interface IUserLogin {
+  email: string;
+  password: string;
+}
+
+export interface IAuthResponse {
+  user: IUserResponse;
+  token: string;
+}
+
+export interface IJwtPayload {
+  id: string;
+  iat?: number;
+  exp?: number;
+}
