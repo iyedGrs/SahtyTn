af11cdae2624aa04b0fcb001a16f9c76c739f6c3
iyed8 <164577765+iyedGrs@users.noreply.github.com>
2025-07-16 21:37:42 +0100

    fixed import

--- Back/user-service/src/routes/auth.ts
+++ Back/user-service/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { register, login } from "../controllers/auth-controller";
+import { register, login, currentUser } from "../controllers/auth-controller";
 const router: Router = express.Router();
 
 // REGISTER
